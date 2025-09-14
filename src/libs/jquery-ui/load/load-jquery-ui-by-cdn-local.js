5cfc7b006e88c03aa31363f88c18c8c56b94bbea
Antonio <antonicut@gmail.com>
2025-09-14 18:09:21 +0200

    Remove archivos y librerias innecesarios

--- src/libs/jquery-ui/load/load-jquery-ui-by-cdn-local.js
+++ src/libs/jquery-ui/load/load-jquery-ui-by-cdn-local.js
@@ -1,9 +1,110 @@
-//  ---------- --------------------------------------------
+//  -------------------------------------------------------
 //  ----------  /load-jquery-ui-by-cdn-local.js  ----------
-//  ---------- --------------------------------------------
+//  -------------------------------------------------------
 
 
-export function loadJQueryUIByCdnOLocal( cdnJQueryUI, localJQueryUI ) {
+
+export function loadJQueryUIByCdnOLocal(cdnJQueryUI = null, localJQueryUI) {
+    
+    
+    return new Promise((resolve, reject) => {
+        
+        console.log('\n');
+        console.warn("Cargando jQuery UI...");
+        
+        
+        //  -----  Si NO se pasó CDN, cargar directo desde local  -----
+        if (!cdnJQueryUI || !cdnJQueryUI.srcCdn) {
+            
+            console.warn("No se definió CDN de jQuery UI, cargando desde local...");
+            
+            const localScript = document.createElement("script");
+            localScript.src = localJQueryUI;
+
+            localScript.onload = () => {
+                
+                if (window.jQuery && jQuery.ui) {
+                    console.warn("jQuery UI cargado correctamente desde local");
+                    resolve(jQuery);
+                } else {
+                    reject(new Error("jQuery UI no se cargó correctamente desde local"));
+                }
+            };
+
+            localScript.onerror = () => reject(new Error("Error al cargar jQuery UI localmente"));
+            document.head.appendChild(localScript);
+            
+            return;
+
+        }
+
+
+        
+        //  -----  SI HAY CDN definido, intentar cargarlo  -----
+        const { 
+            srcCdn, 
+            integrity, 
+            crossOrigin, 
+            referrerPolicy 
+        } = cdnJQueryUI;
+
+        const script = document.createElement("script");
+        
+        script.src = srcCdn;
+        
+        if (integrity) 
+            script.integrity = integrity;
+
+        if (crossOrigin) 
+            script.crossOrigin = crossOrigin;
+
+        if (referrerPolicy) 
+            script.referrerPolicy = referrerPolicy;
+
+
+        script.onload = () => {
+            
+            if (window.jQuery && jQuery.ui) {
+                console.warn("jQuery UI cargado desde CDN");
+                resolve(jQuery);
+            } else {
+                reject(new Error("jQuery UI no cargado correctamente desde CDN"));
+            }
+
+        };
+
+
+        script.onerror = () => {
+            
+            console.error("Error al cargar jQuery UI desde CDN. Intentando cargar localmente...");
+            
+            const localScript = document.createElement("script");
+            localScript.src = localJQueryUI;
+
+            localScript.onload = () => {
+                
+                if (window.jQuery && jQuery.ui) {
+                    console.warn("jQuery UI cargado desde local");
+                    resolve(jQuery);
+                } else {
+                    reject(new Error("jQuery UI no se cargó correctamente desde local"));
+                }
+
+            };
+
+            localScript.onerror = () => reject(new Error("Error al cargar jQuery UI localmente"));
+            document.head.appendChild(localScript);
+        };
+
+        document.head.appendChild(script);
+
+    });
+
+}
+
+
+/*
+export function loadJQueryUIByCdnOLocal( cdnJQueryUI = null, localJQueryUI ) {
     
     const {
         srcCdn,
@@ -61,3 +162,4 @@ export function loadJQueryUIByCdnOLocal( cdnJQueryUI, localJQueryUI ) {
 
     });
 }
+*/
--- src/main.js
+++ src/main.js
@@ -6,11 +6,10 @@
 */
 
 
-//import { cdnJQuery_3_6_3 } from "/01-curso-jquery/src/libs/jquery/cdn/cdn-jquery-3.6.3.js";
 import { cdnJQuery_4_0_0 } from "/src/libs/jquery/cdn/cdn-jquery-4.0.0.js";
 import { loadJQueryByCdnOLocal } from "/src/libs/jquery/load/load-jquery-by-cdn-local.js";
 
-import { cdnJQueryUI_1_14_1 } from "/src/libs/jquery-ui/cdn/cdn-jquery-ui-1.14.1.js";
+//import { cdnJQueryUI_1_14_1 } from "/src/libs/jquery-ui/cdn/cdn-jquery-ui-1.14.1.js";
 import { loadJQueryUIByCdnOLocal } from "/src/libs/jquery-ui/load/load-jquery-ui-by-cdn-local.js";
 
 import { spaWithMethodLoadFromJQueryPlugins } from "/src/plugins/spa-with-method-load-from-jquery/jquery.spa-with-method-load-from-jquery.js";
@@ -22,8 +21,8 @@ const cdnJQuery = cdnJQuery_4_0_0;
 const localJQuery = "/src/libs/jquery/local/jquery-4.0.0-beta.min.js";
 
 //  -----  Carga de jQuery UI  -----
-const cdnJQueryUI = cdnJQueryUI_1_14_1;
-const localJQueryUI = "/src/libs/jquery-ui/local/jquery-ui-1.14.1.min.js";
+const cdnJQueryUI = '';
+const localJQueryUI = "/src/libs/jquery-ui/local/jquery-ui-1.14.1-only-draggable.min.js";
 
 
 //  -------------------------------------
@@ -42,7 +41,7 @@ loadJQueryByCdnOLocal( cdnJQuery, localJQuery )
         //  -----  cargamos jQueryUI  -----
         loadJQueryUIByCdnOLocal( cdnJQueryUI, localJQueryUI )
         .then( $ => {
-        
+                    
             if (!$.ui) { 
                 console.log('\n');
                 throw new Error("jQuery UI no se cargó correctamente.");
